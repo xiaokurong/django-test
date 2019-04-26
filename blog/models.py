2076bc8eb74d9197e5d05f36d8ceac4555c71f82
Robin Liu <all-share@163.com>
2019-04-26 18:15:04 +0800

    添加评论
    Signed-off-by: Robin Liu <all-share@163.com>

--- blog/models.py
+++ blog/models.py
@@ -40,3 +40,17 @@ class Post(models.Model):
                              self.slug])
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post,related_name='comments',on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name,self.post)
