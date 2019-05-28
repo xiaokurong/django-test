d4a55d840beef320a78ca22883e8edae762dde02
Robin Liu <all-share@163.com>
2019-05-28 17:46:27 +0800

    添加表单
    Signed-off-by: Robin Liu <all-share@163.com>

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Question,Choice
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -25,5 +26,17 @@ def results(request,question_id):
     return HttpResponse(response % question_id)
 
 def vote(request,question_id):
-    return HttpResponse('you are voting on question %s. ' % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{'question': question, 'error_message': 'You didnot select a choice.'})
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question': question})
 
