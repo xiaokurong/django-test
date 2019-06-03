cc02a7af74bc0c6945ea9e30f7675843aa2363a3
robin home <robin@163.com>
2019-06-03 23:20:35 +0800

    Signed-off-by: robin home <robin@163.com>

--- saltapi/views.py
+++ saltapi/views.py
@@ -13,14 +13,14 @@ def saltapicmd(request):
         hostnames=request.POST.get('hostnames')
         commands=request.POST.get('commands')
         try:
-            sapi=saltapi.SaltAPI(url='https//172.20.20.70:8000',username='saltapi',password='saltapi')
+            sapi=saltapi.SaltAPI(url='https://172.20.20.70:8000/',username='saltapi',password='saltapi')
             salt_client=hostnames
             salt_method='cmd.run'
             salt_params = commands
             reinfos = sapi.salt_command(salt_client,salt_method,salt_params)
             return render(request,'saltapi/saltapirun.html',{'login_err': reinfos})
-        except Exception:
-            return render(request,'saltapi/saltapirun.html',{'login_err': 'error'})
+        except Exception as error:
+            return render(request,'saltapi/saltapirun.html',{'login_err': error})
 
     else:
         return render(request,'saltapi/saltapirun.html')
