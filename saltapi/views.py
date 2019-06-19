cc3d9a21db775b2744e3043417d983bbf78e0b9f
Robin Liu <all-share@163.com>
2019-06-19 15:27:16 +0800

    添加获取所有salt客户端
    Signed-off-by: Robin Liu <all-share@163.com>

--- saltapi/views.py
+++ saltapi/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from . import saltapi
 from .models import ServerInfo,UserInfo,UserPriv,ServerGroup
+from collections import defaultdict
 
 
 
@@ -39,6 +40,7 @@ def server(request):
 
     try:
         server_set = ServerInfo.objects.all()
+
     except ServerInfo.DoesNotExist:
         render(request,'saltapi/server.html',{'server_set': server_set, })
 
@@ -81,31 +83,38 @@ def insert(request):
 def refresh(request):
     try:
         gsapi = saltapi.SaltAPI(url='https://172.20.20.70:8000/', username='saltapi', password='saltapi')
-        gsalt_client = 'xx'
-        gsalt_method = 'disk.usage'
-        all_list={}
+        gsalt_client = '*'
+        gsalt_method = 'grains.items'
+        all_list= defaultdict(dict)
 
         all_grains = gsapi.salt_command(gsalt_client, gsalt_method)
-        return render(request, 'saltapi/refresh.html', {'refresh': all_grains})
-    except Exception as error:
-        render(request,'saltapi/refresh.html',{'refresh': error})
 
-    #     for i in all_grains.keys():
-    #         all_list[i]['salt_name']= all_grains[i]['id']
-    #         all_list[i]['server_name'] = all_grains[i]['nodename']
-    #         all_list[i]['cpu'] = all_grains[i]['cpu_model']
-    #         all_list[i]['cpu_core'] = all_grains[i]['num_cpus']
-    #         all_list[i]['system'] = all_grains[i]['os'] + ' ' + all_grains[i]['osrelease']
-    #         all_list[i]['ip_addr'] = str(all_grains[i]['ipv4']).replace("'127.0.0.1',","")
-    #         all_list[i]['ram'] = all_grains[i]['mem_total']
-    #         disk_all=gsapi.salt_command(all_grains[i]['id'],'disk.usage')
-    #         all_disk=0
-    #         for j in disk_all.keys():
-    #             all_disk += disk_all[j]['available']
-    #         all_list[i]['disk']=all_disk
-    #
+    #     return render(request, 'saltapi/refresh.html', {'all_grains': all_grains,})
     # except Exception as error:
-    #     return render(request, 'saltapi/refresh.html', {'refresh': error})
+    #     render(request,'saltapi/refresh.html',{'refresh': error})
+    #     print(all_grains)
+
+        # for i in all_grains.keys():
+        #     all_list[i]['salt_name']= all_grains[i]['id']
+        #     all_list[i]['server_name'] = all_grains[i]['nodename']
+        #     all_list[i]['cpu'] = all_grains[i]['cpu_model']
+        #     all_list[i]['cpu_core'] = all_grains[i]['num_cpus']
+        #     all_list[i]['system'] = all_grains[i]['os'] + ' ' + all_grains[i]['osrelease']
+        #     all_list[i]['ip_addr'] = str(all_grains[i]['ipv4']).replace("'127.0.0.1',","")
+        #     all_list[i]['ram'] = all_grains[i]['mem_total']
+        #     # disk_all=gsapi.salt_command(all_grains[i]['id'],'disk.usage')
+        #     # all_disk=0
+        #     # for j in disk_all.keys():
+        #     #     all_disk += disk_all[j]['available']
+        #     #
+        #     # all_list[i]['disk']=all_disk
+        # print("************************************************")
+        # print(all_list)
+        # print("************************************************")
+        return render(request, 'saltapi/refresh.html', {'all_grains': all_grains, })
+
+    except Exception as error:
+        return render(request, 'saltapi/refresh.html', {'refresh_error': error})
 
 
 
